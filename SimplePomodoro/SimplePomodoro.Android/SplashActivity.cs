efdc7c1f564928b5c47ead3bc1ff58a8e44bf3ae
dealen <jakub.marcickiewicz@outlook.com>
2021-12-05 02:42:09 +0100

    Splash screen for android project

---
+++ SimplePomodoro/SimplePomodoro.Android/SplashActivity.cs
@@ -0,0 +1,16 @@
+﻿using Android.App;
+using Android.OS;
+
+namespace SimplePomodoro.Droid
+{
+    [Activity(Label = "SplashActivity", Theme = "@style/Splash", MainLauncher = true)]
+    public class SplashActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            StartActivity(typeof(MainActivity));
+            Finish();
+        }
+    }
+}
